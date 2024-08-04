730598153552cae1b62575a15d06862a79e00a23
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2024-08-04 13:13:13 +0300

    complete the upload demo

--- demo/saveuser.php
+++ demo/saveuser.php
@@ -58,14 +58,20 @@
         }
         header($url);
     }else {
+
+        # generate _id
+        $old_id = file_get_contents('ids.txt');
+        $old_id = (int)$old_id;
+        $id = $old_id + 1;
+        file_put_contents('ids.txt', $id);
+
         # get file info --> save it to the server
         $temp_name = $_FILES['image']['tmp_name'];
         $image_name = $_FILES['image']['name'];
-        $image_path="images/{$image_name}";
+        $image_path="images/{$id}.{$ext}";
         $saved=move_uploaded_file($temp_name,$image_path);
         if($saved){
             generate_title("Image uploaded successfully",1,'green');
-            echo "<img src='images/{$image_name}' width='100' height='100'> ";
 
         }else{
             generate_title("Error uploading file",1,'red');
@@ -73,11 +79,7 @@
 
         # prepare data_to_be_save_on_server
 
-        $old_id = file_get_contents('ids.txt');
-        var_dump($old_id);
-        $old_id = (int)$old_id;
-        $id = $old_id + 1;
-        file_put_contents('ids.txt', $id);
+
 
         $data = "{$id}:{$_POST['name']}:{$_POST['email']}:{$_POST['password']}:{$image_path}\n";
 
